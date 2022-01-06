d1ff5801762d6f44b1134619f044d09e7a5053f5
sergey <sergey.nosalyuk@gmail.com>
2022-01-06 21:55:03 +0200

    first commit

---
+++ js/Lesson1HW.js
@@ -0,0 +1,19 @@
+console.log("привет")
+let foo = 0;
+switch(foo) {
+    case -1:
+        console.log('negative 1');
+        break;
+    case 0:
+        console.log(0);
+        break;
+    case 1:
+        console.log(1);
+        break;
+    case 2:
+        console.log(2);
+        break;
+    default:
+        console.log('default');
+    
+}
